8da344cf3eaf266670136135a2ec314ec285a364
cschneider <clement.schneider@manchenumerique.fr>
2024-06-05 12:21:35 +0200

    feat: Hide job offer when you click again

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.1.1] - 2024-06-05
+
+### Added
+    - Hide job offer when you click again
+
 ## [1.0.1] - 2024-06-04
 
 ### Fixed
@@ -16,4 +21,3 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
     - Custom your API link using the BackOffice
     - Display jobs offers from your API link
-
--- wp-offres-emploi-intranet/inc/offres-emploi.js
+++ wp-offres-emploi-intranet/inc/offres-emploi.js
@@ -25,20 +25,33 @@ function afficherDetailOffre(offreDetails) {
 var offreButtons = document.getElementsByClassName('offre-specifique-emploi-intranet');
 for (var i = 0; i < offreButtons.length; i++) {
     offreButtons[i].addEventListener('click', function() {
-        var offreDetails = {
-            intitule: this.getAttribute('data-intitule'),
-            filiere: this.getAttribute('data-filiere'),
-            residence: this.getAttribute('data-lieu-travail'),
-            direction: this.getAttribute('data-direction'),
-            type_recrutement: this.getAttribute('data-type-recrutement'),
-            cadre: this.getAttribute('data-cadre'),
-            date_prise_poste: this.getAttribute('data-date-prise-poste'),
-            remuneration: this.getAttribute('data-remuneration'),
-            mission: this.getAttribute('data-mission'),
-            profil: this.getAttribute('data-profil'),
-            specificitees: this.getAttribute('data-specificitees'),
-            conditions: this.getAttribute('data-conditions')
-        };
-        afficherDetailOffre(offreDetails);
+        var currentlyActive = document.querySelector('.offre-specifique-emploi-intranet.active');
+        var offreDetailElement = document.getElementById('offre-detail');
+
+        if (currentlyActive && currentlyActive !== this) {
+            currentlyActive.classList.remove('active');
+        }
+
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+            offreDetailElement.innerHTML = '';
+        } else {
+            this.classList.add('active');
+            var offreDetails = {
+                intitule: this.getAttribute('data-intitule'),
+                filiere: this.getAttribute('data-filiere'),
+                residence: this.getAttribute('data-lieu-travail'),
+                direction: this.getAttribute('data-direction'),
+                type_recrutement: this.getAttribute('data-type-recrutement'),
+                cadre: this.getAttribute('data-cadre'),
+                date_prise_poste: this.getAttribute('data-date-prise-poste'),
+                remuneration: this.getAttribute('data-remuneration'),
+                mission: this.getAttribute('data-mission'),
+                profil: this.getAttribute('data-profil'),
+                specificitees: this.getAttribute('data-specificitees'),
+                conditions: this.getAttribute('data-conditions')
+            };
+            afficherDetailOffre(offreDetails);
+        }
     });
 }
